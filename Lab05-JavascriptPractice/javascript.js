f3381ee2c22694ba045faeaee94a863319759aa6
Elizapp <41589068+Elizapp@users.noreply.github.com>
2021-06-26 02:35:00 -0500

    Lab05 - JavaScript Practice

--- Lab05-JavascriptPractice/javascript.js
+++ Lab05-JavascriptPractice/javascript.js
@@ -80,70 +80,46 @@ function findLongestWord(words) {
 }
 
 function filterLongWords(words, i) {
-    return words.filter(word => word.length > i);
-}
-
-function computeSumOfSquares(numbers, ) {
-    return numbers.reduce((x, y) => x + Math.pow(y, 2));
-}
-
-function printOddNumbersOnly(numbers) {
-    return numbers.filter(num => num % 2 !== 0);
-}
-
-function computeSumOfSquaresOfEvensOnly(numbers) {
-    return numbers
-        .filter(num => num % 2 === 0)
-        .reduce((x, y) => x + Math.pow(y, 2));
-}
-
-function sumWithReduce(numbers) {
-    return numbers.reduce((x, y) => x + y);
+    return "" + words.filter(word => word.length > i);
 }
 
 function multipliesWithReduce(numbers) {
     return numbers.reduce((x, y) => x * y);
 }
 
-function findSecondBiggest(numbers) {
-    let first = second = Number.MIN_VALUE;
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > first) {
-            second = first;
-            first = numbers[i];
-        } else if (numbers[i] !== first && numbers[i] > second) {
-            second = numbers[i];
-        }
-    }
 
-    return second;
+
+/* max3 takes 3 numbers as arguments and returns the largest */
+function maxOfThree2(a, b, c) {
+    return max(max(a, b), c);
+
 }
 
-function printFibo(n, a, b) {
-    let x = a, y = b, count = n, temp, fibo = [];
-    while (count > 0) {
-        temp = x;
-        x = x + y;
-        y = temp;
-        fibo.push(y);
-        count--;
-    }
-    console.log(fibo)
-}
+
 
 function test() {
-    printResults('1. max(4,5)', 5, max(4, 5))
-    printResults('2. maxOfThree(8,26, 27)', 27, maxOfThree(8,26, 27))
-    printResults('3. isVowel("a")', 'true', isVowel('a'))
-    printResults('4.1. sum([45,5,6,7])', 63, sum([45,5,6,7]))
-    printResults('4.2. multiplies([45,5,6,7])', 9450, multiply([45,5,6,7]))
-    printResults('5. reverse("ratset gaj test")', "tset jag testar", reverse("ratset gaj test"))
-    printResults('6. findLongestWord(["love", "thrut", "Tim", "Today is a good time"])', 20, findLongestWord(["love", "thrut", "Tim", "Today is a good time"]))
-    printResults('7. filterLongWords(["John", "Pedro", "Lanjahet", "Bart", "Kamala"], 4)', ["Pedro","Lanjahet","Kamala"], filterLongWords(["John", "Pedro", "Lanjahet", "Bart", "Kamala"], 4))
+    console.log(
+    "Expected output of  " + myFunctionTest(' max(4,5)', 5, max(4, 5))
+   + "\nExpected output of  " + myFunctionTest(' maxOfThree(8,26, 27)', 27, maxOfThree(8,26, 27))
+   + "\nExpected output of  " + myFunctionTest(' isVowel("a")', true, isVowel('a'))
+   + "\nExpected output of  " + myFunctionTest(' sum([45,5,6,7])', 63, sum([45,5,6,7]))
+   + "\nExpected output of  " + myFunctionTest(' multiply([45,5,6,7])', 9450, multiply([45,5,6,7]))
+   + "\nExpected output of  " + myFunctionTest(' reverse("ratset gaj test")', "tset jag testar", reverse("ratset gaj test"))
+   + "\nExpected output of  " + myFunctionTest(' findLongestWord(["love", "thrut", "Tim", "Today is a good time"])', 20, findLongestWord(["love", "thrut", "Tim", "Today is a good time"]))
+   + "\nExpected output of  " + myFunctionTest(' filterLongWords(["John", "Pedro", "Lanjahet", "Bart", "Kamala"], 4)', ["Pedro", "Lanjahet", "Kamala"], filterLongWords(["John", "Pedro", "Lanjahet", "Bart", "Kamala"], 4))
+  // + "Expected output of  " + multipliesWithReduce(' multipliesWithReduce([45,5,6,7])', 5, multipliesWithReduce([45,5,6,7]))
+    );
 }
 
-function printResults(no, expected, results) {
-    console.log('------------------\n ' + no)
-    console.log('Expected value: ' + expected)
-    console.log('Result: ' + results)
+function myFunctionTest(no, expected, results) {
+
+    if (expected === results) {
+        return no + " is "  + expected + " TEST SUCCEEDED";
+    } else {
+        return no + " is "  + expected + " TEST FAILED.  Expected " + expected + " found " + results;
+    }
 }
+console.log("Expected output of maxOfThree(5,4,44) is 44  " + myFunctionTest("",44, maxOfThree2(5, 4, 44)));
+console.log("Expected output of maxOfThree(55,4,44) is 55  " + myFunctionTest("",55, maxOfThree2(55, 4, 44)));
+
+console.log("Expected output of maxOfThree(55,4,44) is 55  " + myFunctionTest("",4, maxOfThree2(55, 4, 44)));
